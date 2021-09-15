90ec568806503bda3a4a7bb4abcdabf1a9be99b1
bitko <marcelina.bee@gmail.com>
2021-09-15 21:57:13 +0200

    testy

---
+++ test_web.py
@@ -0,0 +1,22 @@
+# import unittest
+#
+#
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         self.assertEqual(True, False)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+# # test_with_unittest.py
+#
+# from unittest import TestCase
+#
+# class TryTesting(TestCase):
+#     def test_always_passes(self):
+#         self.assertTrue(True)
+#
+#     def test_always_fails(self):
+#         self.assertTrue(False)
+def test_example():
+    assert True
