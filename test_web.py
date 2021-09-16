f8a7f8d40e4738586619c541b65dfecd076f725a
bitko <marcelina.bee@gmail.com>
2021-09-16 21:01:11 +0200

    adding test

--- test_web.py
+++ test_web.py
@@ -1,22 +1,10 @@
-# import unittest
-#
-#
-# class MyTestCase(unittest.TestCase):
-#     def test_something(self):
-#         self.assertEqual(True, False)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
-# # test_with_unittest.py
-#
-# from unittest import TestCase
-#
-# class TryTesting(TestCase):
-#     def test_always_passes(self):
-#         self.assertTrue(True)
-#
-#     def test_always_fails(self):
-#         self.assertTrue(False)
-def test_example():
-    assert True
+from urllib.request import urlopen
+from datetime import date
+
+def test_display():
+    link = "http://127.0.0.1:5000/"
+    f = urlopen(link)
+    myfile = f.read()
+    content = myfile.decode("utf-8")
+    current_date = str(date.today())
+    assert current_date in content
