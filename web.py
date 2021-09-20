c4830664d18c6273ee47f9f7882996a6f8d354ca
bitko <marcelina.bee@gmail.com>
2021-09-20 19:59:51 +0200

    adding comments

--- web.py
+++ web.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import date
 from flask import render_template
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+comments = []
 
 @app.route("/")
 def hello_world():
     today = date.today()
-    return render_template('index.html', name=today)
+    return render_template('index.html', date=today, comments=comments)
+
+@app.route("/add-comment", methods=['POST'])
+def add_comment():
+    comment = request.form['comment']
+    comments.append(comment)
+    return redirect('/')
