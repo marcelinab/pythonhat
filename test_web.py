7a8a84a24c9548db4b9fdb8359790554090f57e3
bitko <marcelina.bee@gmail.com>
2021-09-20 20:07:59 +0200

    adding comments

--- test_web.py
+++ test_web.py
@@ -1,6 +1,9 @@
 from urllib.request import urlopen
 from datetime import date
 
+import requests
+
+
 def test_display():
     link = "http://127.0.0.1:5000/"
     f = urlopen(link)
@@ -8,3 +11,13 @@ def test_display():
     content = myfile.decode("utf-8")
     current_date = str(date.today())
     assert current_date in content
+
+def test_comment():
+    comment = "test"
+    link = "http://127.0.0.1:5000/"
+    myobj = {'comment': comment}
+    requests.post(link+"/add-comment", data=myobj)
+    f = urlopen(link)
+    myfile = f.read()
+    content = myfile.decode("utf-8")
+    assert comment in content
